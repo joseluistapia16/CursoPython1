0b8fdd15c23755b4fa75725ee7aefcd3c40e2ac3
joseluistapia16 <joseluistapia09@gmail.com>
2023-06-14 21:48:51 -0500

    Clase 2

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ if edad>25 and edad < 65:
 if edad>64:
     print("Adulto mayor")
 
-'''
+
 cliente = input("Cliente:")
 producto = input("Producto:")
 precio = float(input("Precio:"))
@@ -62,6 +62,62 @@ cantidad = int(input("Cantidad:"))
 subtotal =(precio * cantidad)
 iva = (subtotal*0.12)
 total = (subtotal+iva)
+descuento5=(total*0.05)
+descuento= (subtotal-descuento5)
+ivad= (descuento*0.12)
 res = "Subtotal:"+str(round(subtotal,2))+"\nIva:"+str(round(iva,2))\
-      +"\nTotal a pagar:"+str(round(total))
-print(res)
++"\nTotal a pagar:"+str(round(total,2))
+#print(res)
+if subtotal>=100:
+    descuento5 = (subtotal * 0.05)
+    descuento = (subtotal - descuento5)
+    ivad = (descuento * 0.12)
+    totalpago = (descuento + ivad)
+    print("EL Decuento es:"+str(round(descuento,2)))
+    print("Iva:" +str(ivad))
+    print("Total a pagar:"+str(round(totalpago,2)))
+
+c= 1
+s=0
+ac = 0
+while c< 10:
+    if c%2!=0:
+       print(c)
+       s= s+1
+       ac = ac + c
+    c= c+1
+print("Total impares es "+str(s))
+print("Total acumulado es "+str(ac))
+
+c =0
+ac=0
+for i in range(1,11):
+    if i % 2==0:
+      ac = ac + i
+      print(i)
+      c= c+1
+print("Total de pares  es "+str(c))
+print("Total acumulado es "+str(ac))
+
+datos =("MARIA",20,100, True,300)
+#print(datos[0])
+for i in range(len(datos)):
+    print(datos[i])
+print("Tamaño de tupla",len(datos))
+datos =("MARIA",20)
+print(datos)
+'''
+lista = []
+lista.append("DIAZ")
+lista.append(300)
+lista.append(True)
+lista.append("POO")
+lista.append(54)
+print(len(lista))
+print(lista)
+lista[1]=500
+#lista.pop(1)
+del lista[2]
+print(lista)
+lista.clear()
+print(len(lista))
